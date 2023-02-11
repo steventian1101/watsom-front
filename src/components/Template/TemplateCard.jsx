3ff0d44aa75f25d29d832680ff5c2fa773adf65a
steventian1101 <steventian1101@gmail.com>
2023-02-11 11:20:45 -0800

    fixed the template link

--- src/components/Template/TemplateCard.jsx
+++ src/components/Template/TemplateCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useTranslation } from "react-i18next";
 import { Card} from 'flowbite-react';
 import TemplateSearch from './TemplateSearch';
+import { useNavigate } from 'react-router-dom'
 
 import { HiOutlineSearch, HiOutlineHashtag, HiOutlineMail } from "react-icons/hi";
 import { HiOutlineSquares2X2, HiOutlineComputerDesktop } from "react-icons/hi2";
@@ -13,9 +14,11 @@ import { FcAdvertising } from "react-icons/fc"
 function TemplateCard({title, content, link, icon}) {
   const { t } = useTranslation();
 
+  const navigate = useNavigate();
+
   return (
     <div className="col-span-1" style={{minHeight: "50px"}}>
-      <Card className="text-start cursor-pointer h-full" href="/a">
+      <Card className="text-start cursor-pointer h-full" href="" onClick={()=>navigate(`/template/${link}`)}>
         {
           icon == "all" ? 
           <HiOutlineSquares2X2 
