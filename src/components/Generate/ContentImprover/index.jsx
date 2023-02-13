2964c6a3aab91a6647359b648b87be70747b88a1
steventian1101 <steventian1101@gmail.com>
2023-02-12 22:10:26 -0800

    completed the content improver front cosmetic

--- src/components/Generate/ContentImprover/index.jsx
+++ src/components/Generate/ContentImprover/index.jsx
@@ -1,5 +1,7 @@
+import { Textarea } from 'flowbite-react';
 import React, { useState } from 'react';
 import { useTranslation } from "react-i18next";
+import ToneSelect from '../ToneSelect';
 
 function ContentImprover({
 }) {
@@ -7,7 +9,24 @@ function ContentImprover({
 
   return (
     <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden bg-gray-100" style={{height: " calc(100vh - 10rem) "}}>
-      
+      <div style={{textAlign: "-webkit-center"}}>
+        <div className='w-2/3 text-start py-2'>
+          <div className='font-bold py-2'>
+            Content to Improve
+          </div>
+          <Textarea 
+            className='bg-white w-full'
+            multiline={true}
+            rows={5}
+          />
+        </div>
+        <div className='w-2/3 text-start py-2'>
+          <div className='font-bold py-2'>
+            Tone
+          </div>
+          <ToneSelect />
+        </div>
+      </div>
     </div>
   );
 }
