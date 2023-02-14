f84aeebf7804a4545fc45f0cd071b079b2a6a2b2
steventian1101 <steventian1101@gmail.com>
2023-02-14 14:03:13 -0800

    added live server

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,2 @@
-const DEV_MODE = true;
-export const API_BASE = DEV_MODE == true?'http://localhost:3333':'';
-export const API_URL = DEV_MODE == true ?'http://localhost:8006':'';
+const DEV_MODE = false;
+export const API_BASE = DEV_MODE == true?'http://localhost:3333':'https://watsom.onrender.com';
